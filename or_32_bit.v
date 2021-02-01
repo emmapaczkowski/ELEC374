afff4188c6a01bd3d420b6bccab631e5e75c3dc0
varnikaagupta <43092064+varnikaagupta@users.noreply.github.com>
2021-02-01 11:13:10 -0500

    Update or_32_bit.v

--- or_32_bit.v
+++ or_32_bit.v
@@ -1,15 +1,15 @@
 `timescale 1ns / 1ps
 
 module or_32_bit(
-	input wire [31:0] Ra,
-	input wire [31:0] Rb,
-	output wire [31:0] Rz
+	input wire [31:0] RegA,
+	input wire [31:0] RegB,
+	output wire [31:0] RegZ
 	);
 	
 	genvar i;
 	generate
 		for (i=0; i<32; i=i+1) begin : loop
-			assign Rz[i] = ((Ra[i])|(Rb[i]));
+			assign RegZ[i] = ((RegA[i])|(RegB[i]));
 		end
 	endgenerate
 endmodule
