469f3695f714f66f50399f4515b5d841d4b9b4da
SydneyCaulfeild <47483016+SydneyCaulfeild@users.noreply.github.com>
2021-02-01 11:21:33 -0500

    Update add_32_bit.v

--- add_32_bit.v
+++ add_32_bit.v
@@ -8,6 +8,7 @@ CLA16 CLA2(.Ra(Ra[31:16]), .Rb(Rb[31:16]), .cin(cout1), .sum(sum[31:16]), .cout(
 
 endmodule
 
+// 16-bit carry look-ahead adder
 module CLA16(input wire [15:0] Ra, input wire [15:0] Rb, input wire cin, output wire [15:0] sum, output wire cout);
 wire cout1,cout2,cout3;
 
@@ -17,9 +18,11 @@ CLA4 CLA3(.Ra(Ra[11:8]), .Rb(Rb[11:8]), .cin(co==ut2), .sum(sum[11:8]), .cout(co
 CLA4 CLA4(.Ra(Ra[15:12]), .Rb(Rb[15:12]), .cin(cout3), .sum(sum[15:12]), .cout(cout));
 
 endmodule
- 
+
+// 4-bit carry look-ahead adder
 module CLA4(input wire [3:0] Ra, input wire [3:0] Rb, input wire cin, output wire[3:0] sum, output wire cout);
-wire [3:0] P,G,c;
+// Creating wires for Propoate and Generate
+	wire [3:0] P,G,c;
 
 	assign P=Ra^Rb;	
 	assign G=Ra&Rb;
