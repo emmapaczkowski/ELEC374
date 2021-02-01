47e5bb0f3f9adb0dba8f0dd5c978ee48f9529b44
Emma Paczkowski <45154853+emmapaczkowski@users.noreply.github.com>
2021-02-01 11:01:59 -0500

    Update negate_32_bit.v

--- negate_32_bit.v
+++ negate_32_bit.v
@@ -8,6 +8,6 @@ module negate_32_bit(
 	wire [31:0] temp; 
 	wire cout;
 	not_32_bit not_op(.Ra(Ra),.Rz(temp));
-	add_32_bit add_op(.Ra(temp), .Rb(32'd1),.cin(1'd0),.sum(Rz),.cout(cout));
+	add_32_bit add_op(.Ra(temp), .Rb(32'd1), .cin(1'd0),.sum(Rz), .cout(cout));
 	
 endmodule
