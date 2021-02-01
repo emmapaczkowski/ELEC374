81c85d891a81e31326445bc3daa05120b262382a
SydneyCaulfeild <47483016+SydneyCaulfeild@users.noreply.github.com>
2021-02-01 10:57:57 -0500

    Update sub_32_bit.v

--- sub_32_bit.v
+++ sub_32_bit.v
@@ -2,6 +2,6 @@
 
 module SUB(input wire [31:0] Ra, input wire [31:0] Rb, input wire cin, output wire [31:0] sum, output wire cout);
 	wire [31:0] temp; 
-	negate_32_bit neg(.Ra(Rb),.Rz(temp));
+	negate_32_bit negate(.Ra(Rb),.Rz(temp));
 	add_32_bit add(.Ra(Ra), .Rb(temp),.cin(cin),.sum(sum),.cout(cout));
 endmodule
