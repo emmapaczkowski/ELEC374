1ac1ee863f8a3286ec9e48d7a8d8a2bfe4a02241
Emma Paczkowski <45154853+emmapaczkowski@users.noreply.github.com>
2021-03-15 10:42:33 -0400

    Update select_encode_logic.v

--- select_encode_logic.v
+++ select_encode_logic.v
@@ -1,6 +1,17 @@
 `timescale 1ns/10ps
 
-module select_encode_logic(input [31:0] instruction, input Gra, input Grb, input Grc, input Rin, input Rout, input BAout, output [4:0] opcode, output [31:0] C_sign_extended, output [15:0] RegIn, output [15:0] RegOut, output wire [3:0] decoderInput);
+module select_encode_logic(input [31:0] instruction,
+			   input Gra,
+			   input Grb,
+			   input Grc,
+			   input Rin,
+			   input Rout, 
+			   input BAout,
+			   output [4:0] opcode, 
+			   output [31:0] C_sign_extended,
+			   output [15:0] RegIn, 
+			   output [15:0] RegOut, 
+			   output wire [3:0] decoderInput);
 	wire [15:0] decoderOutput;
 	
 	assign decoderInput = (instruction[26:23]&{4{Gra}}) | (instruction[22:19]&{4{Grb}}) | (instruction[18:15]&{4{Grc}});
